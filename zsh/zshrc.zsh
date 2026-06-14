0190ff902f3b84628eef484fc1df8496dfa45250
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2026-06-14 18:38:39 +0200

    improve zsh prompt

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -5,6 +5,20 @@ setopt prompt_subst
 zstyle ':vcs_info:git:*' formats '%B%F{yellow}%b%f'
 zstyle ':vcs_info:*' enable git
 
+# Let build_prompt own the env segment instead of the tools munging PROMPT
+# themselves (build_prompt rebuilds PROMPT every prompt and would clobber it).
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+export CONDA_CHANGEPS1=false
+
+# Name of the active dev environment (python venv, conda, etc.), or empty.
+prompt_env_name() {
+  if [[ -n $VIRTUAL_ENV ]]; then
+    echo "${VIRTUAL_ENV:t}"                                   # python venv / poetry / uv
+  elif [[ -n $CONDA_DEFAULT_ENV ]]; then
+    echo "${CONDA_DEFAULT_ENV:t}"                             # conda / mamba
+  fi
+}
+
 # utf8
 LC_CTYPE=en_US.UTF-8
 LC_ALL=en_US.UTF-8
@@ -16,7 +30,9 @@ build_prompt() {
   local exit_code=$?
   vcs_info
   local -a parts
+  local env_name="$(prompt_env_name)"
   (( exit_code ))             && parts+="%B%F{red}${exit_code}%f%b"   # exit code
+  [[ -n $env_name ]]          && parts+="%B%F{green}(${env_name})%f%b"  # dev env (venv/conda)
   [[ -n $SSH_CONNECTION ]]    && parts+="%B%F{magenta}%m%f%b"         # hostname (SSH only)
   [[ -n $vcs_info_msg_0_ ]]   && parts+="$vcs_info_msg_0_"            # git branch
   parts+="%B%F{blue}%5~%f%b"                                          # working dir
